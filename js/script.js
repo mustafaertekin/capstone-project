7f7e65d6af6f6c3df4540f747b2f9cbd28502b32
mustafaertekin <mustafa014@gmail.com>
2017-10-28 16:09:41 +0200

    css zoom anim added

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+    $(".threeColumnImages").on("mouseover", function{
+           $(this).addClass("zoom");                    
+                               
+    });
+
+
+});
