139409f2ae1386ee3ca52da9bdf61e2c11345970
mustafaertekin <mustafa014@gmail.com>
2017-10-29 15:32:39 +0100

    added google-map
    <script> into html &
    myMap function into script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
-$(document).ready(function() {
-    $('#myForm').validator();
-    
-    
 
-});
+function myMap() {
+  var mapCanvas = document.getElementById("map");
+  var mapOptions = {
+    center: new google.maps.LatLng(47.3, 7.9), zoom: 10
+  };
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+}
+
 
